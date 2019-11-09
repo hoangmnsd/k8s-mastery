e73de54e215f80c14af916fa10109aa2711b522a
Ubuntu <ubuntu@ip-172-31-28-137.ec2.internal>
2019-11-09 07:45:17 +0000

    update changes

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+//        fetch('http://localhost:8080/sentiment', {
+        fetch('http://54.157.202.96:31180/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
